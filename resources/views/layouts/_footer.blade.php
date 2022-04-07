68a3003077c08160b27b8482b426a808520f69c4
MysticalDEvil <jinpen7981@outlook.com>
2022-04-07 21:58:22 +0800

    Basic page structure

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-start">
+      <img class="brand-icon" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github"/>
+      <a href="https://github.com/MysticalDevil" target="_blank">MysticalDEvil</a> My github page
+      <span style="color: #e27575; font-size: 14px;">♥</span>
+    </p>
+
+    <p class="float-end"><a href="devil2gamma@gamil.com">联系我</a></p>
+  </div>
+</footer>
